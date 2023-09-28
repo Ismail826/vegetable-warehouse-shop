8a22d8ebaa75f380808b9046b25e9651677320f4
Ismail826 <user@gmail.com>
2023-09-29 00:32:47 +0600

    banner part work done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,10 +2,10 @@ import React from 'react';
 import banner from '../../../images/banner.png';
 const Banner = () => {
     return (
-        <div className="container mx-auto bg-blue-500 text-white text-center p-8 flex">
-           <div className='w-50 mx-auto items-center'>
-                <h1 className="text-2xl font-bold">Welcome to our Website!</h1>
-                <p className="mt-2">Discover amazing features and services.</p>
+        <div className=" mx-auto bg-indigo-100 text-white text-center p-8 flex">
+           <div className='w-50 mx-auto items-center mt-48'>
+                <h1 className="text-2xl font-bold text-black">Welcome to our Website!</h1>
+                <p className="mt-2 text-black">Discover amazing features and services.</p>
                 <button className="mt-4 bg-white text-blue-500 py-2 px-6 rounded-full">
                     Learn More
                 </button>
